3999e311156a9a98b905c7b68b001c31a6636fdc
tumelescasse <98033025+tumelescasse@users.noreply.github.com>
2026-07-28 09:51:42 +0200

    Move Virvolt section to dedicated page

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -9,8 +9,14 @@
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
 import { Route as rootRouteImport } from './routes/__root'
+import { Route as VirvoltRouteImport } from './routes/virvolt'
 import { Route as IndexRouteImport } from './routes/index'
 
+const VirvoltRoute = VirvoltRouteImport.update({
+  id: '/virvolt',
+  path: '/virvolt',
+  getParentRoute: () => rootRouteImport,
+} as any)
 const IndexRoute = IndexRouteImport.update({
   id: '/',
   path: '/',
@@ -19,28 +25,39 @@ const IndexRoute = IndexRouteImport.update({
 
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
+  '/virvolt': typeof VirvoltRoute
 }
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
+  '/virvolt': typeof VirvoltRoute
 }
 export interface FileRoutesById {
   __root__: typeof rootRouteImport
   '/': typeof IndexRoute
+  '/virvolt': typeof VirvoltRoute
 }
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/'
+  fullPaths: '/' | '/virvolt'
   fileRoutesByTo: FileRoutesByTo
-  to: '/'
-  id: '__root__' | '/'
+  to: '/' | '/virvolt'
+  id: '__root__' | '/' | '/virvolt'
   fileRoutesById: FileRoutesById
 }
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
+  VirvoltRoute: typeof VirvoltRoute
 }
 
 declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
+    '/virvolt': {
+      id: '/virvolt'
+      path: '/virvolt'
+      fullPath: '/virvolt'
+      preLoaderRoute: typeof VirvoltRouteImport
+      parentRoute: typeof rootRouteImport
+    }
     '/': {
       id: '/'
       path: '/'
@@ -53,6 +70,7 @@ declare module '@tanstack/react-router' {
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
+  VirvoltRoute: VirvoltRoute,
 }
 export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
